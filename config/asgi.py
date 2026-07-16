8c36b4bdf399fab5cc604b317d9329baa05adc09
Vipin <vipincnaduvalath@gmail.com>
2026-07-17 01:36:38 +0530

    fix: initialize Django before loading Channels routing

--- config/asgi.py
+++ config/asgi.py
@@ -9,27 +9,24 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-
-from .routing import websocket_urlpatterns
-
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
     "config.settings",
 )
 
+from django.core.asgi import get_asgi_application
+
 django_asgi_app = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from .routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-
         "websocket": AuthMiddlewareStack(
-            URLRouter(
-                websocket_urlpatterns
-            )
+            URLRouter(websocket_urlpatterns)
         ),
     }
 )
