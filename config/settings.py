a253d1725f35c1afc3485ddfe4eef7b6389bcf0c
Vipin <vipincnaduvalath@gmail.com>
2026-07-17 00:01:49 +0530

    refactor: move Django configuration to environment variables

--- config/settings.py
+++ config/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 LOGIN_URL='login'
 AUTH_USER_MODEL = "users.User"
 
